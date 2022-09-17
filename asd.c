0f3398f8e2a8a5633c4acceb93048fd181ee9983
Molidos <113747112+Molidos@users.noreply.github.com>
2022-09-17 00:51:21 -0300

    Add files via upload

---
+++ asd.c
@@ -0,0 +1,9 @@
+
+            #include <stdio.h>
+        
+            int main(){
+                printf("Ola Mundo!");
+                system("PAUSE");
+            
+                return 1;
+            } 
